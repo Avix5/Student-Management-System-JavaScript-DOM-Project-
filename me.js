67cb0a0dd7da6b5b5d0eec1d07e7d5a955d29afb
Abhishek Yadav <167422303+Avix5@users.noreply.github.com>
2026-03-14 11:05:02 +0530

    Implement edit functionality for student entries
    Added functionality to edit existing student entries in the table.

--- me.js
+++ me.js
@@ -6,23 +6,56 @@ let studentEmail= document.getElementById("studentEmail");
 let studentcource= document.getElementById("studentCourse");
 let btn=document.getElementById("submitBtn");
 // let clearBtn=getElementById("clearBtn");
+let editRow=null;
+
 
 
 
 btn.addEventListener("click",function(e){
      e.preventDefault();
-    // console.log(studentname.value,studentid.value,studentEmail.value,studentcource.value);
-    let tbody=document.getElementById("studentTable");
-    let tr= document.createElement("tr");
+        let tbody=document.getElementById("studentTable");
+         if(editRow){
+        editRow.children[0].innerText=studentname.value;
+        editRow.children[1].innerText=studentid.value;
+        editRow.children[2].innerText=studentEmail.value;
+        editRow.children[3].innerText=studentcource.value;
+        editRow = null;
+        btn.innerText="Add student"
+        
+    }else{
+            let tr= document.createElement("tr");
     tr.innerHTML=`
     <td>${studentname.value}</td>
     <td>${studentid.value}</td>
     <td>${studentEmail.value}</td>
     <td>${studentcource.value}</td>
     <td><button class="del">Delete</button></td>
-    <td><button class="del">Edit</button></td>
+    <td><button class="edit">Edit</button></td>
     `
     tbody.appendChild(tr);
+
+
+    let editbtn= tr.querySelector(".edit");
+    editbtn.addEventListener("click",function(e){
+    e.preventDefault();
+    console.log("clicked edit")
+
+    studentname.value=tr.children[0].innerText;
+    studentid.value = tr.children[1].innerText;
+    studentEmail.value = tr.children[2].innerText;
+    studentcource.value = tr.children[3].innerText;
+    editRow=tr
+    btn.innerText="Save Changes"
+    
+   
+
+});
+    }
+    
     form.reset();
-})
+ 
+
+});
+
+
 
